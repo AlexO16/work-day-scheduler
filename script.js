5569b318e9c57c270443d494510dcc36b7c26076
Alex Oleynik <alexoleynik74@gmail.com>
2022-09-25 11:17:28 -0500

    working on saveBtn

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ var saveBtn = $('#saveBtn');
 
 
 //Display current date and time in the header
-function displayTime() {
-  var currentDate = moment().format('LLLL');
-  dateTimeDisplay.text(currentDate);
-}
+// function displayTime() {
+//   var currentDate = moment().format('LLLL');
+//   dateTimeDisplay.text(currentDate);
+// }
 
-setInterval(displayTime, 1000);
+// setInterval(displayTime, 1000);
+
+//Jquery save button to local storage
+saveBtn.on("click", function() {
+    var count = $(this).siblings(".hour").text();
+    var schedule = $(this).siblings(".event").val();
+    localStorage.setItem(count, schedule);
+});
 
